62fe87e0a5e5ca51e8641aa8cbf44e2880cc6458
developedbyangel <angelfrancis1111@gmail.com>
2022-09-24 14:34:53 +0530

    all sections designed

--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,14 @@ const checkImage=(event)=>
 {
     var elmt=document.getElementsByClassName("slide-in")[0];
     var scrolled = window.scrollY+elmt.offsetHeight*1.5;
-    if(scrolled>elmt.offsetTop)
+    if(scrolled>elmt.offsetParent.offsetTop+elmt.offsetTop)
     {
         elmt.style.transform="translateX(0)";
         elmt.style.opacity="100"
     }
     else
     {
-        elmt.style.transform="translateX(-30vw)";
+        elmt.style.transform="translateX(30vw)";
         elmt.style.opacity="0"
     }
 }
