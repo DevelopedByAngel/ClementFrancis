25b627f9eaa882f5a638a5186f3a4172d80e232b
developedbyangel <angelfrancis1111@gmail.com>
2022-09-25 19:56:17 +0530

    formatting css

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,14 @@
-const checkImage=(event)=>
-{
-    if(window.outerWidth>768)
-    {
-            var elmt=document.getElementsByClassName("slide-in")[0];
-    var scrolled = window.scrollY+elmt.offsetHeight*1.5;
-    if(scrolled>elmt.offsetParent.offsetTop+elmt.offsetTop)
-    {
-        elmt.style.transform="translateX(0)";
-        elmt.style.opacity="100"
+const checkImage = (event) => {
+  if (window.outerWidth > 768) {
+    var elmt = document.getElementsByClassName("slide-in")[0];
+    var scrolled = window.scrollY + elmt.offsetHeight * 1.5;
+    if (scrolled > elmt.offsetParent.offsetTop + elmt.offsetTop) {
+      elmt.style.transform = "translateX(0)";
+      elmt.style.opacity = "100";
+    } else {
+      elmt.style.transform = "translateX(30vw)";
+      elmt.style.opacity = "0";
     }
-    else
-    {
-        elmt.style.transform="translateX(30vw)";
-        elmt.style.opacity="0"
-    }
-
-    }
-}
-window.addEventListener("scroll",checkImage);
+  }
+};
+window.addEventListener("scroll", checkImage);
