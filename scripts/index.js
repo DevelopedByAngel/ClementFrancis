64e59012d3b0b25f8e09c9976cab33c9168cfd91
developedbyangel <angelfrancis1111@gmail.com>
2022-09-25 19:39:21 +0530

    about me content and mobile

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 const checkImage=(event)=>
 {
-    var elmt=document.getElementsByClassName("slide-in")[0];
+    if(window.outerWidth>768)
+    {
+            var elmt=document.getElementsByClassName("slide-in")[0];
     var scrolled = window.scrollY+elmt.offsetHeight*1.5;
     if(scrolled>elmt.offsetParent.offsetTop+elmt.offsetTop)
     {
@@ -11,6 +13,8 @@ const checkImage=(event)=>
     {
         elmt.style.transform="translateX(30vw)";
         elmt.style.opacity="0"
+    }
+
     }
 }
 window.addEventListener("scroll",checkImage);
